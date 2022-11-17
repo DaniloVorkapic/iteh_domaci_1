0b3b8602d633f403a8934b883d73754a5839d613
DaniloVorkapic <vorkapic.danilo13@gmail.com>
2022-11-17 09:44:21 +0100

    kreiran model

---
+++ model/Telefon.php
@@ -0,0 +1,28 @@
+<?php
+
+class Telefon{
+    private $id;
+    private $model;
+    private $description;
+    private $price;
+    private $user;
+ 
+
+
+
+    public function __construct($id=null,$model=null,$description=null,$price=null,$user=null ) 
+    {
+        $this->id=$id;
+        $this->model=$model;
+        $this->description=$description;
+        $this->price=$price;
+       
+        $this->user=$user; 
+
+    }
+
+  
+
+}
+
+?>
